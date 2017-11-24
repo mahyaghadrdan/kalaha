08b4edb2251666cf2a97656e30dd4e88e2c96c20
mahya <mahyaghadrdan@gmail.com>
2017-11-23 17:33:50 -0800

    Verify IDDF

--- src/ai/Agent.java
+++ src/ai/Agent.java
@@ -17,6 +17,7 @@ public class Agent {
     private int MIN = 1;
     private int MAX = 2;
     private long stopTime;
+    private int bestMove = -1;
 
     public Agent(int player, long stoptime) {
         if (player == 1) {
@@ -27,44 +28,47 @@ public class Agent {
         this.stopTime = stoptime;
     }
 
-    public int getBestMove(GameState currGameState, int currentDepth) {
-        int bestMove = -1;
-        int bestScoreAtDepth = -1000;
-
+    public int getBestMove(GameState currGameState, int currentDepth, int maxDepth) {
         int player = currGameState.getNextPlayer();
 
-        if (currGameState.gameEnded() || DEPTH > currentDepth) {
+        if (currGameState.gameEnded() || currentDepth == maxDepth) {
             return evaluate(currGameState, player);
         }
 
         long currentTime = System.currentTimeMillis();
 
-        while (DEPTH > currentDepth && currentTime > this.stopTime) {
-            for (int i = 1; i <= 6; i++) {
+        if ((this.stopTime - currentTime) > 0) {
+            int bestScoreAtDepth = -1000;
+            int bestMove = -1;
 
-                if (currGameState.moveIsPossible(i)) {
+            while (maxDepth > currentDepth) {
 
-                    GameState clone = currGameState.clone();
-                    clone.makeMove(i);
+                for (int i = 1; i <= 6; i++) {
 
-                    int moveScore = getBestMove(clone, currentDepth + 1);
+                    if (currGameState.moveIsPossible(i)) {
 
-                    if (player == MAX && bestScoreAtDepth < moveScore) {
-                        bestScoreAtDepth = moveScore;
-                        bestMove = i;
-                    }
+                        GameState clone = currGameState.clone();
+                        clone.makeMove(i);
 
-                    if (player == MIN && moveScore < bestScoreAtDepth) {
-                        bestScoreAtDepth = moveScore;
-                        bestMove = i;
+                        int moveScore = getBestMove(clone, currentDepth + 1, maxDepth);
+
+                        if (player == MAX && bestScoreAtDepth < moveScore) {
+                            bestScoreAtDepth = moveScore;
+                            bestMove = i;
+                        }
+
+                        if (player == MIN && moveScore < bestScoreAtDepth) {
+                            bestScoreAtDepth = moveScore;
+                            bestMove = i;
+                        }
                     }
                 }
-            }
 
-            DEPTH++;
+                this.bestMove = bestMove;
+            }
         }
 
-        return bestMove;
+        return this.bestMove;
     }
 
     public int evaluate(GameState currentGameState, int player) {
